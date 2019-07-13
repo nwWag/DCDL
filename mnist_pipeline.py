383218ee36cd0be741c36f2254157410f1d515e4
nwWag <nicolas_wagner@gmx.de>
2019-07-13 16:05:52 +0200

    Update mnist_pipeline.py

--- mnist_pipeline.py
+++ mnist_pipeline.py
@@ -51,10 +51,10 @@ def evaluate(arch):
 print("Training", flush=True)
 archs = []
 
-archs.append(network("baseline-bn_before-pool_before",avg_pool=False, real_in=False,
+archs.append(network("Block5",avg_pool=False, real_in=False,
                     lr=1E-4, batch_size=2**8, activation=binarize_STE,
                      pool_by_stride=False, pool_before=True, pool_after=False,
-                     skip=True, pool_skip=True,
+                     skip=False, pool_skip=False,
                      bn_before=True, bn_after=False, ind_scaling=False
                      ))
 archs[-1].training(train_nn, label_train_nn, val, label_val)
